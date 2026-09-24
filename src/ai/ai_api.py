d11008c105eb9e5728bd608267fa17527b4fef7d
Ping Xuan <2603059@sit.singaporetech.edu.sg>
2026-09-24 22:22:15 +0800

    chore(ai): remove unused file and update gitignore

--- src/ai/ai_api.py
+++ src/ai/ai_api.py
@@ -1,20 +1,36 @@
-import requests 
-import json
+import os
+from openrouter import OpenRouter
 
-response = requests.post(
-  url="https://openrouter.ai/api/v1/chat/completions",
-  headers={
-    "Authorization": "Bearer <OPENROUTER_API_KEY>",
-    "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
-    "X-OpenRouter-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
-  },
-  data=json.dumps({
-    "model": "~openai/gpt-sol-latest",
-    "messages": [
-      {
-        "role": "user",
-        "content": "What is the meaning of life?"
-      }
-    ]
-  })
-) 
+
+def send_request(input_text="Tell me what is in a computer in one sentence"):
+  with OpenRouter(
+      api_key=os.getenv("OPENROUTER_API_KEY", ""),
+  ) as open_router:
+      res = open_router.chat.send(
+          model= "dots-studio/dots-3-note-preview:free",
+          messages=[
+              {"content": input_text, "role": "user"}
+          ],
+          # response_format={
+          #    "type" : "json_schema",
+          #    "json_schema": {
+          #       "type": "object",
+          #       "properties":{
+          #          "confidence level": {
+          #             "type": "number",
+          #             "description": "How correct is the reply"
+          #          },
+          #          "message": {
+          #             "type": "string",
+          #             "description": "Reply"
+          #          }
+          #       }
+          #    }
+          # },
+          stream=False,
+      )
+      # print(res.choices[0].message.content)
+      print(res)
+      return res
+
+send_request()
