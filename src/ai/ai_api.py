b7b55ed210e41bdf7fddba5f1bddccefc1265b6b
Ping Xuan <2603059@sit.singaporetech.edu.sg>
2026-09-25 19:17:04 +0800

    chore(ai): update dependency list

--- src/ai/ai_api.py
+++ src/ai/ai_api.py
@@ -2,7 +2,7 @@ import os
 from openrouter import OpenRouter
 
 
-def send_request(input_text="Tell me what is in a computer in one sentence"):
+def send_request(input_text="What is a tree in one sentence?"):
   with OpenRouter(
       api_key=os.getenv("OPENROUTER_API_KEY", ""),
   ) as open_router:
@@ -11,22 +11,22 @@ def send_request(input_text="Tell me what is in a computer in one sentence"):
           messages=[
               {"content": input_text, "role": "user"}
           ],
-          # response_format={
-          #    "type" : "json_schema",
-          #    "json_schema": {
-          #       "type": "object",
-          #       "properties":{
-          #          "confidence level": {
-          #             "type": "number",
-          #             "description": "How correct is the reply"
-          #          },
-          #          "message": {
-          #             "type": "string",
-          #             "description": "Reply"
-          #          }
-          #       }
-          #    }
-          # },
+        #   response_format={
+        #      "type" : "json_schema",
+        #      "json_schema": {
+        #         "type": "object",
+        #         "properties":{
+        #            "confidence level": {
+        #               "type": "number",
+        #               "description": "How correct is the reply"
+        #            },
+        #            "message": {
+        #               "type": "string",
+        #               "description": "Reply"
+        #            }
+        #         }
+        #      }
+        #   },
           stream=False,
       )
       # print(res.choices[0].message.content)
